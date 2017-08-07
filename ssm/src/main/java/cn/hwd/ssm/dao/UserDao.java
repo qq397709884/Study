7bbc26d2a8d5737b5ec9cd7cee1cc50bd9ceadae
vinegar93 <vinegar93@163.com>
2017-08-07 15:39:10 +0800

    MapperScannerConfigurer

--- ssm/src/main/java/cn/hwd/ssm/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package cn.hwd.ssm.dao;
-
-import cn.hwd.ssm.bean.User;
-
-public interface UserDao {
-	
-	public User findUserById(int id); 
-	
-}
--- ssm/src/main/java/cn/hwd/ssm/service/impl/UserServiceImpl.java
+++ ssm/src/main/java/cn/hwd/ssm/service/impl/UserServiceImpl.java
@@ -4,18 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cn.hwd.ssm.bean.User;
-import cn.hwd.ssm.dao.UserDao;
+import cn.hwd.ssm.mapper.UserMapper;
 import cn.hwd.ssm.service.UserService;
 
 @Service("userService")
 public class UserServiceImpl implements UserService {
 	
 	@Autowired
-    private UserDao userDao;
+    private UserMapper userMapper;
 	
 	@Override
 	public User findUserById(int id) throws Exception {
-        return userDao.findUserById(id);
+        return userMapper.findUserById(id);
 	}
 
 }
